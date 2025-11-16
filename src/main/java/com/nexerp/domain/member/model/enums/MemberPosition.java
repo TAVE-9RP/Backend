8cc176809726e5b704cb3af8598c361140e403c3
kwak513 <kwak513@hufs.ac.kr>
2025-11-16 20:03:59 +0900

    feature: 회원의 '직급' ENUM 파일 추가

---
+++ src/main/java/com/nexerp/domain/member/model/enums/MemberPosition.java
@@ -0,0 +1,20 @@
+package com.nexerp.domain.member.model.enums;
+
+public enum MemberPosition {
+    INTERN("인턴"),
+    ASSISTANT_MANAGER("주임"),
+    MANAGER("대리"),
+    SENIOR_MANAGER("과장"),
+    DEPARTMENT_HEAD("부장"),
+    OWNER("오너");
+
+    private final String description;
+
+    MemberPosition(String description) {
+        this.description = MemberPosition.this.description;
+    }
+
+    public String getKoreanName() {
+        return description;
+    }
+}
