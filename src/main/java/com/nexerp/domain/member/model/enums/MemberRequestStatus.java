64f9e9fed79ebf5e2852281db180c912a0aabf86
kwak513 <kwak513@hufs.ac.kr>
2025-11-16 20:04:29 +0900

    feature: 회원의 '가입 상태' ENUM 파일 추가

---
+++ src/main/java/com/nexerp/domain/member/model/enums/MemberRequestStatus.java
@@ -0,0 +1,18 @@
+package com.nexerp.domain.member.model.enums;
+
+public enum MemberRequestStatus {
+    PENDING("요청대기"),
+    APPROVED("승인"),
+    REJECTED("거절");
+
+    private final String description;
+
+    MemberRequestStatus(String description) {
+        this.description = MemberRequestStatus.this.description;
+    }
+
+    public String getKoreanName() {
+        return description;
+    }
+
+}
