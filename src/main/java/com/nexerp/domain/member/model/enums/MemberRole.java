7f14fa1df38bb21350e63e67fad622c0b721a421
kwak513 <kwak513@hufs.ac.kr>
2025-11-17 13:09:43 +0900

    comment: Member 권한 ENUM 주석 추가

--- src/main/java/com/nexerp/domain/member/model/enums/MemberRole.java
+++ src/main/java/com/nexerp/domain/member/model/enums/MemberRole.java
@@ -1,7 +1,9 @@
 package com.nexerp.domain.member.model.enums;
 
+// Member 권한 ENUM
 public enum MemberRole {
-    ALL("읽기, 쓰기 권한"),
+    ALL("오너 권한"),
+    WRITE("읽기, 쓰기 권한"),
     READ("읽기 전용 권한");
 
     private final String description;
