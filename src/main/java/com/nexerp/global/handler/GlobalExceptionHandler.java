f18f0deef7235f1113f3786b228d8c5e2927f234
minsub <hyohyosubsub@naver.com>
2025-11-20 20:37:01 +0900

    refactor: swagger test

--- src/main/java/com/nexerp/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/nexerp/global/handler/GlobalExceptionHandler.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Hidden
 @Slf4j
-@RestControllerAdvice
+// @RestControllerAdvice
 public class GlobalExceptionHandler {
 
   /**
