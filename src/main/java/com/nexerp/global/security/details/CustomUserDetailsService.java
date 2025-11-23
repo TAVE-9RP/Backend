5307f0d334fc67d2cc39f046784fd5171efeb507
kwak513 <kwak513@hufs.ac.kr>
2025-11-24 08:44:30 +0900

    feature: reissueToken에서 Authentication 생성 로직을 CustomUserDetailsService로 분리

--- src/main/java/com/nexerp/global/security/details/CustomUserDetailsService.java
+++ src/main/java/com/nexerp/global/security/details/CustomUserDetailsService.java
@@ -2,7 +2,11 @@ package com.nexerp.global.security.details;
 
 import com.nexerp.domain.member.model.entity.Member;
 import com.nexerp.domain.member.repository.MemberRepository;
+import com.nexerp.global.common.exception.BaseException;
+import com.nexerp.global.common.exception.GlobalErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -27,5 +31,19 @@ public class CustomUserDetailsService implements UserDetailsService {
         else{
             throw new UsernameNotFoundException("해당하는 사용자를 찾을 수 없습니다: " + loginId);
         }
+    }
+
+    public Authentication createAuthenticationById(String memberId) {
+        // DB에서 Member 조회
+        Member member = memberRepository.findById(Long.valueOf(memberId))
+          .orElseThrow(() -> new BaseException(GlobalErrorCode.NOT_FOUND, "회원이 존재하지 않습니다."));
+
+        // DB에서 가져온 Member 정보로 CustomUserDetails를 생성하여 Authentication 객체를 만듦
+        CustomUserDetails userDetails = new CustomUserDetails(member);
+
+        // CustomUserDetails의 권한 정보를 Authentication 객체에 담기
+        return new UsernamePasswordAuthenticationToken(
+          userDetails, null, userDetails.getAuthorities()
+        );
     }
 }
