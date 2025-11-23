472492ec02480cb18e58e4e3484e9fe855a78d61
kwak513 <kwak513@hufs.ac.kr>
2025-11-24 08:55:05 +0900

    fix: final 키워드 추가 및 불필요한 필드 제거

--- src/main/java/com/nexerp/domain/member/model/response/MemberAuthResponseDto.java
+++ src/main/java/com/nexerp/domain/member/model/response/MemberAuthResponseDto.java
@@ -2,34 +2,17 @@ package com.nexerp.domain.member.model.response;
 
 import com.nexerp.domain.member.model.enums.MemberDepartment;
 import com.nexerp.domain.member.model.enums.MemberPosition;
-import com.nexerp.domain.member.model.enums.MemberRole;
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 // 로그인 성공 시 반환 DTO
 public class MemberAuthResponseDto {
     // JWT Access Token
-    private String accessToken;
+    private final String accessToken;
 
     // JWT Refresh Token
-    private String refreshToken;
+    private final String refreshToken;
 
-    // Access Token 만료 시간
-    private Long accessTokenExpirationTime;
-
-    // 빠른 초기 화면 분기를 위해서
-    // 부서
-    private MemberDepartment department;
-
-    // 직급
-    private MemberPosition position;
 }
