cc863c166e1c1a89626b719e8936af2bf80bc45d
zldzldzz <zldzldzz@gs.anyang.ac.kr>
2025-11-25 10:17:08 +0900

    feature: 회사 관련 도메인 SecurityConfig에 추가

--- src/main/java/com/nexerp/global/config/SecurityConfig.java
+++ src/main/java/com/nexerp/global/config/SecurityConfig.java
@@ -19,42 +19,47 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    // 비밀번호 해시화
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  // 비밀번호 해시화
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/member/signup").permitAll()
-                        .requestMatchers("/member/login").permitAll()
-                        .requestMatchers("/member/reissue").permitAll()
-                        .requestMatchers("/swagger-ui/").permitAll()
-                        .requestMatchers("/v3/api-docs/").permitAll()
-                        .anyRequest().authenticated()
-                );
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+      .csrf(csrf -> csrf.disable())
+      .sessionManagement(session ->
+        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      )
+      .authorizeHttpRequests(authorize -> authorize
+        .requestMatchers("/member/signup").permitAll()
+        .requestMatchers("/member/login").permitAll()
+        .requestMatchers("/member/reissue").permitAll()
+        .requestMatchers("/swagger-ui/").permitAll()
+        .requestMatchers("/v3/api-docs/").permitAll()
 
-        http.addFilterBefore(
-                jwtAuthenticationFilter,
-                UsernamePasswordAuthenticationFilter.class
-        );
-        return http.build();
-    }
+        // 회사 관련 도메인
+        .requestMatchers("/companies/**").permitAll()
 
-    // AuthenticationManager 등록
-    // AuthenticationManager는 사용자가 입력한 id/pw를 받고, CustomUserDetailsService로 DB에서 사용자 정보 조회하고, 비밀번호 검증
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+        .anyRequest().authenticated()
+      );
+
+    http.addFilterBefore(
+      jwtAuthenticationFilter,
+      UsernamePasswordAuthenticationFilter.class
+    );
+    return http.build();
+  }
+
+  // AuthenticationManager 등록
+  // AuthenticationManager는 사용자가 입력한 id/pw를 받고, CustomUserDetailsService로 DB에서 사용자 정보 조회하고, 비밀번호 검증
+  @Bean
+  public AuthenticationManager authenticationManager(
+    AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
 
 }
