86e6cd8c00e23ac517e4ef1fdcbaf6d3d7fdebc2
zldzldzz <zldzldzz@gs.anyang.ac.kr>
2025-11-25 12:30:43 +0900

    feature: CompanyController 수정
    - 토큰을 인증 기능 추가
    - 회사 관련 스웨거 문서화

--- src/main/java/com/nexerp/domain/company/controller/CompanyController.java
+++ src/main/java/com/nexerp/domain/company/controller/CompanyController.java
@@ -5,9 +5,16 @@ import com.nexerp.domain.company.model.response.CompanyCreateResponse;
 import com.nexerp.domain.company.model.response.CompanySearchResponse;
 import com.nexerp.domain.company.service.CompanyService;
 import com.nexerp.global.common.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,12 +25,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/companies")
 @RequiredArgsConstructor
+@Tag(name = "회사 관련 API", description = "생성 / 키워드를 통한 조회(리스트)")
 public class CompanyController {
 
-  //hasRole 추가 예정
   private final CompanyService companyService;
 
   @PostMapping
+  @PreAuthorize("hasRole('OWNER')")
+  // 스웨거 전용 어노테이션 Operation
+  // jwtAuth이거는 변경 가능
+  @Operation(
+    summary = "회사 생성 api",
+    description = "**오너 권한이 있는 인증 토큰 필요** 회사 이름 중복 불가합니다. "
+      + " 회사 이름, 업종 필수입니다.",
+    security = @SecurityRequirement(name = "jwtAuth"),
+    requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+      description = "추가 입력 정보",
+      required = true,
+      content = @Content(
+        mediaType = "application/json",
+        schema = @Schema(implementation = CompanyCreateRequest.class),
+        examples = @ExampleObject(
+          name = "회사 생성 예시",
+          value = """
+            {
+              "name": "테스트 회사",
+              "industryType": "물류/운송",
+              "description": "중복 테스트용 회사",
+              "imagePath": "http://exmple"
+            }
+            """
+        )
+      )
+    )
+  )
   public BaseResponse<CompanyCreateResponse> createCompany(
     @Valid @RequestBody CompanyCreateRequest companyCreateRequest) {
     CompanyCreateResponse companyCreateResponse = companyService.createCompany(
@@ -32,6 +67,12 @@ public class CompanyController {
   }
 
   @GetMapping
+  @PreAuthorize("isAuthenticated()")
+  @Operation(summary = "키워드를 통한 회사 조회 api",
+    description = "**로그인 된 사용자만 사용 가능, keyword 파라미터 필수** keyword=\"\"의 경우 모든 회사 반환 "
+      + "/ 키워드 포함 회사가 없는 경우 빈리스트 반환",
+    security = @SecurityRequirement(name = "jwtAuth")
+  )
   public BaseResponse<List<CompanySearchResponse>> searchCompanies(
     @RequestParam("keyword") String keyword) {
 
