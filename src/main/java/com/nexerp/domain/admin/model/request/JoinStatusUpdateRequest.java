e71e46391e7fd963d8cce3d55ca938e9bdb374b5
minsub <hyohyosubsub@naver.com>
2025-11-26 01:37:42 +0900

    feature: JoinStatusUpdateRequest 추가

---
+++ src/main/java/com/nexerp/domain/admin/model/request/JoinStatusUpdateRequest.java
@@ -0,0 +1,11 @@
+package com.nexerp.domain.admin.model.request;
+
+import com.nexerp.domain.member.model.enums.MemberRequestStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class MemberRequestStatusUpdateRequest {
+  @NotNull(message = "변경할 가입 상태는 필수입니다.")
+  private MemberRequestStatus newStatus;
+}
