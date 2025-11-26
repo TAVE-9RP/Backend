e5dcb34bbb44d483a8603aa3f5d31e9cfdd0e82b
minsub <hyohyosubsub@naver.com>
2025-11-26 20:11:40 +0900

    refactor: Enum DTO에서는 String으로 처리

--- src/main/java/com/nexerp/domain/admin/model/request/JoinStatusUpdateRequest.java
+++ src/main/java/com/nexerp/domain/admin/model/request/JoinStatusUpdateRequest.java
@@ -1,11 +1,15 @@
 package com.nexerp.domain.admin.model.request;
 
 import com.nexerp.domain.member.model.enums.MemberRequestStatus;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class JoinStatusUpdateRequest {
+
   @NotNull(message = "변경할 가입 상태는 필수입니다.")
-  private MemberRequestStatus newStatus;
+  @Enumerated(EnumType.STRING)
+  private String newStatus;
 }
