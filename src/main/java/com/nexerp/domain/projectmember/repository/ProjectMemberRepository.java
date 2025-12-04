1d56f0ebc34f6cae57772937c6c626af2b9f3ed2
minsub <hyohyosubsub@naver.com>
2025-12-04 22:04:18 +0900

    feature: ProjectMemberRepository 추가

---
+++ src/main/java/com/nexerp/domain/projectmember/repository/ProjectMemberRepository.java
@@ -0,0 +1,7 @@
+package com.nexerp.domain.projectmember.repository;
+
+import com.nexerp.domain.projectmember.model.entity.ProjectMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
+}
