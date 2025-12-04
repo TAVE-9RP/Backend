2b10b4cc0a5d3359d8312c3ee6f604cd23f6b66a
minsub <hyohyosubsub@naver.com>
2025-12-04 22:04:33 +0900

    refactor: 담당자 추가에 따른 DTO 수정

--- src/main/java/com/nexerp/domain/project/model/request/ProjectCreateRequest.java
+++ src/main/java/com/nexerp/domain/project/model/request/ProjectCreateRequest.java
@@ -2,8 +2,11 @@ package com.nexerp.domain.project.model.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -25,4 +28,8 @@ public class ProjectCreateRequest {
   @JsonFormat(pattern = "yyyy-MM-dd")
   @NotNull(message = "목표 종료일을 입력하세요")
   private LocalDate projectExpectedEndDate;
+
+  // 프로젝트 담당자 리스트
+  @NotEmpty(message = "담당자를 최소 1명 이상 지정해주세요.")
+  private List<Long> assigneeIds;
 }
