436fd18d59774d1f63c1ab5da561ba1dfabe4e58
kwak513 <kwak513@hufs.ac.kr>
2025-12-05 22:44:44 +0900

    delete: 프로젝트의 담당자인지 여부 확인 로직 삭제

--- src/main/java/com/nexerp/domain/projectmember/repository/ProjectMemberRepository.java
+++ src/main/java/com/nexerp/domain/projectmember/repository/ProjectMemberRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
 
-  boolean existsByProjectIdAndMemberId(Long projectId, Long memberId);
 }
