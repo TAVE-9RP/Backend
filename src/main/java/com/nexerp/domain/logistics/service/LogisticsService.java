bd277df74935b1e1ce80cfeb94beeb54db94de3f
zldzldzz <zldzldzz@gs.anyang.ac.kr>
2025-12-19 11:07:01 +0900

    feature: 출하 물품 추가 - LogisticsService

--- src/main/java/com/nexerp/domain/logistics/service/LogisticsService.java
+++ src/main/java/com/nexerp/domain/logistics/service/LogisticsService.java
@@ -1,15 +1,23 @@
 package com.nexerp.domain.logistics.service;
 
+import com.nexerp.domain.item.model.entity.Item;
+import com.nexerp.domain.item.repository.ItemRepository;
 import com.nexerp.domain.logistics.model.entity.Logistics;
+import com.nexerp.domain.logistics.model.request.LogisticsItemsCreateRequest.LogisticsItemDetail;
 import com.nexerp.domain.logistics.model.request.LogisticsUpdateRequest;
 import com.nexerp.domain.logistics.model.response.LogisticsSearchResponse;
 import com.nexerp.domain.logistics.repository.LogisticsRepository;
+import com.nexerp.domain.logisticsItem.model.entity.LogisticsItem;
+import com.nexerp.domain.logisticsItem.repository.LogisticsItemRepository;
 import com.nexerp.domain.member.service.MemberService;
 import com.nexerp.domain.project.model.entity.Project;
 import com.nexerp.domain.project.service.ProjectService;
 import com.nexerp.global.common.exception.BaseException;
 import com.nexerp.global.common.exception.GlobalErrorCode;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +29,8 @@ public class LogisticsService {
   private final MemberService memberService;
   private final ProjectService projectService;
   private final LogisticsRepository logisticsRepository;
+  private final ItemRepository itemRepository;
+  private final LogisticsItemRepository logisticsItemRepository;
 
   @Transactional(readOnly = true)
   public List<LogisticsSearchResponse> searchLogisticsByMemberId(Long memberId) {
@@ -87,6 +97,47 @@ public class LogisticsService {
     }
 
     logistics.requestApproval();
+  }
+
+  @Transactional
+  public void addItems(Long memberId, Long logisticsId, List<LogisticsItemDetail> itemRequests) {
+    Logistics logistics = logisticsRepository.findWithProjectAndCompanyById(logisticsId)
+      .orElseThrow(() -> new BaseException(GlobalErrorCode.NOT_FOUND, "출하 업무를 찾을 수 없습니다."));
+
+    // 회사 검증
+    Long memberCompanyId = memberService.getCompanyIdByMemberId(memberId);
+    if (!memberCompanyId.equals(logistics.getProject().getCompany().getId())) {
+      throw new BaseException(GlobalErrorCode.FORBIDDEN, "다른 회사의 출하 업무에는 접근할 수 없습니다.");
+    }
+
+    // 요청의 물품 id 추출
+    List<Long> itemIds = itemRequests.stream()
+      .map(LogisticsItemDetail::getItemId)
+      .toList();
+
+    List<Item> foundItems = itemRepository.findAllById(itemIds);
+
+    Map<Long, Item> itemMap = foundItems.stream()
+      .collect(Collectors.toMap(Item::getId, Function.identity()));
+
+    List<LogisticsItem> logisticsItems = itemRequests.stream()
+      .map(detail -> {
+        Item item = itemMap.get(detail.getItemId());
+
+        if (item == null) {
+          throw new BaseException(GlobalErrorCode.NOT_FOUND,
+            "ID: " + detail.getItemId() + " 물품을 찾을 수 없습니다.");
+        }
+        return LogisticsItem.create(
+          logistics,
+          item,
+          detail.getLogisticsTargetedQuantity()
+        );
+
+      })
+      .toList();
+
+    logisticsItemRepository.saveAll(logisticsItems);
   }
 
 }
