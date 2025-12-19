c6fac417beb7cfd968430b9b405d011c0be431cc
zldzldzz <zldzldzz@gs.anyang.ac.kr>
2025-12-19 15:33:08 +0900

    refactor: LogisticsItemsCreateRequest 수정
    - LogisticsItemDetail이었지만 출하와 구분하기 위해서 필드명 수정
    - 목표 수량 NotNull 추가

--- src/main/java/com/nexerp/domain/logistics/model/request/LogisticsItemsCreateRequest.java
+++ src/main/java/com/nexerp/domain/logistics/model/request/LogisticsItemsCreateRequest.java
@@ -19,17 +19,18 @@ public class LogisticsItemsCreateRequest {
 
   @NotEmpty(message = "최소 하나 이상의 물품을 선택해야 합니다.")
   @Valid
-  private List<LogisticsItemDetail> items;
+  private List<CreateLogisticsItemDetail> items;
 
   @Getter
   @NoArgsConstructor
   @AllArgsConstructor
-  public static class LogisticsItemDetail {
+  public static class CreateLogisticsItemDetail {
 
     @NotNull(message = "물품(Inventory) ID는 필수입니다.")
-    private Long ItemId;
+    private Long itemId;
 
     @Min(value = 1, message = "수량은 1개 이상이어야 합니다.")
+    @NotNull(message = "수량은 필수입니다.")
     private Long logisticsTargetedQuantity;
   }
 }
