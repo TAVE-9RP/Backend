844408ee2fb2f4856c7e9b9e4da46274a9098029
zldzldzz <zldzldzz@gs.anyang.ac.kr>
2025-12-19 10:54:17 +0900

    feature: 출하 업무 수정 로직 추가 - LogisticsController

--- src/main/java/com/nexerp/domain/logistics/controller/LogisticsController.java
+++ src/main/java/com/nexerp/domain/logistics/controller/LogisticsController.java
@@ -1,14 +1,19 @@
 package com.nexerp.domain.logistics.controller;
 
+import com.nexerp.domain.logistics.model.request.LogisticsUpdateRequest;
 import com.nexerp.domain.logistics.model.response.LogisticsSearchResponse;
 import com.nexerp.domain.logistics.service.LogisticsService;
 import com.nexerp.global.common.response.BaseResponse;
 import com.nexerp.global.security.details.CustomUserDetails;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +32,20 @@ public class LogisticsController {
     List<LogisticsSearchResponse> result = logisticsService.searchLogisticsByMemberId(memberId);
 
     return BaseResponse.success(result);
+  }
+
+  @PatchMapping("/{logisticsId}")
+  @PreAuthorize("hasPermission('LOGISTICS', 'WRITE')")
+  public BaseResponse<Void> updateLogisticsInfo(
+    @AuthenticationPrincipal CustomUserDetails userDetails,
+    @PathVariable Long logisticsId,
+    @Valid @RequestBody LogisticsUpdateRequest logisticsUpdateRequest
+  ) {
+    Long memberId = userDetails.getMemberId();
+
+    logisticsService.updateLogisticsInfo(memberId, logisticsId, logisticsUpdateRequest);
+
+    return BaseResponse.success();
   }
 
 }
