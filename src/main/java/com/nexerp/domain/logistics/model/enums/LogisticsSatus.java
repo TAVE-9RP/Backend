0ff60bbea356738f2fe6604b4985272332338065
zldzldzz <zldzldzz@gs.anyang.ac.kr>
2025-12-19 10:37:22 +0900

    feature: 엔티티의 이넘 추가
    - LogisticsSatus: 업무 상태
    - LogisticsProcessingStatus 물품 상태

---
+++ src/main/java/com/nexerp/domain/logistics/model/enums/LogisticsSatus.java
@@ -0,0 +1,11 @@
+package com.nexerp.domain.logistics.model.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum LogisticsSatus {
+  ASSIGNED,
+  APPROVAL_PENDING,
+  IN_PROGRESS,
+  COMPLETED;
+}
---
+++ src/main/java/com/nexerp/domain/logisticsItem/model/enums/LogisticsProcessingStatus.java
@@ -0,0 +1,10 @@
+package com.nexerp.domain.logisticsItem.model.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum LogisticsProcessingStatus {
+  PENDING,
+  IN_PROGRESS,
+  COMPLETED
+}
