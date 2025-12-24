0aa1777bc5ee0730ed54ccff7827d80bb3f8661e
minsub <hyohyosubsub@naver.com>
2025-12-25 02:45:00 +0900

    feature: S3 파일 적재 테스트

---
+++ src/main/java/com/nexerp/batch/controller/EtlTestController.java
@@ -0,0 +1,21 @@
+package com.nexerp.batch.controller;
+
+import com.nexerp.batch.service.S3TestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/etl/test")
+public class EtlTestController {
+
+  private final S3TestService s3TestService;
+
+  @GetMapping("/upload")
+  public String testUpload() throws Exception {
+    s3TestService.uploadTestFile();
+    return "Uploaded!";
+  }
+}
