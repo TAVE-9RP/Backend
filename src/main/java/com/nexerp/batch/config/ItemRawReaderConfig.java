cf8c92597778d6ccd7c802664e259b91658f61b1
minsub <hyohyosubsub@naver.com>
2025-12-27 01:24:03 +0900

    feature: itemReader 추가

---
+++ src/main/java/com/nexerp/batch/config/ItemRawReaderConfig.java
@@ -0,0 +1,29 @@
+package com.nexerp.batch.config;
+
+import com.nexerp.batch.dto.ItemRawRow;
+import com.nexerp.batch.repository.BatchRepository;
+import com.nexerp.domain.item.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class ItemRawReaderConfig {
+
+  private final BatchRepository batchRepository;
+
+  @Bean
+  public RepositoryItemReader<ItemRawRow> itemRawReader() {
+    RepositoryItemReader<ItemRawRow> reader = new RepositoryItemReader<>();
+    reader.setRepository(batchRepository);
+    reader.setMethodName("findItemRawRows");
+    reader.setPageSize(1000);
+    reader.setSort(Map.of("id", Sort.Direction.ASC));
+    return reader;
+  }
+}
