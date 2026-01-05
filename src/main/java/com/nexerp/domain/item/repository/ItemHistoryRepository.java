2c7e65c599b5146ae0bd4ea7a52c8da4adfee99f
zldzldzz <zldzldzz@gs.anyang.ac.kr>
2026-01-05 20:33:01 +0900

    feature: 물품 id를 통한 기록 시간 별 조회 쿼리 추가

--- src/main/java/com/nexerp/domain/item/repository/ItemHistoryRepository.java
+++ src/main/java/com/nexerp/domain/item/repository/ItemHistoryRepository.java
@@ -1,8 +1,10 @@
 package com.nexerp.domain.item.repository;
 
 import com.nexerp.domain.item.model.entity.ItemHistory;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemHistoryRepository extends JpaRepository<ItemHistory, Long> {
 
+  List<ItemHistory> findByItemIdOrderByProcessedAtDesc(Long itemId);
 }
