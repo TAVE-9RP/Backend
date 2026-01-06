3184e26386457277641fdf7d828ec7b2f14b0939
minsub <hyohyosubsub@naver.com>
2026-01-06 21:43:55 +0900

    chore: 운영 DB 테스트 위해 일시 삭제

--- src/main/java/com/nexerp/batch/kpi/model/repository/KpiSnapshotRepository.java
+++ src/main/java/com/nexerp/batch/kpi/model/repository/KpiSnapshotRepository.java
@@ -7,6 +7,5 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 public interface KpiSnapshotRepository extends JpaRepository<KpiSnapshot, Long> {
-  // 회사 ID와 날짜로 조회 (Insert, Update)
   Optional<KpiSnapshot> findByCompanyIdAndSnapshotDate(Long companyId, LocalDate snapshotDate);
 }
