d150b86ca6c180acf5aea2604a92e1de1d8eafb3
zldzldzz <wjl100735@gmail.com>
2026-01-09 23:30:24 +0900

    feature: 본인이 속한 입고 리스트 조회 Repository

--- src/main/java/com/nexerp/domain/inventory/repository/InventoryRepository.java
+++ src/main/java/com/nexerp/domain/inventory/repository/InventoryRepository.java
@@ -1,10 +1,21 @@
 package com.nexerp.domain.inventory.repository;
 
 import com.nexerp.domain.inventory.model.entity.Inventory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
   List<Inventory> findAllByProject_Company_Id(Long companyId);
+
+  @Query("""
+      select distinct inv
+      from Inventory inv
+        join inv.project p
+        join p.projectMembers pm
+      where p.company.id = :companyId
+        and pm.member.id = :memberId
+    """)
+  List<Inventory> findAllAssignedToMember(Long companyId, Long memberId);
 }
