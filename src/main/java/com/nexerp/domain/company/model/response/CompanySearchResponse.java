d04506e1bb1f0724aa05389d2e1c8846f2ac40d8
zldzldzz <wjl100735@gmail.com>
2026-01-17 12:35:45 +0900

    feature: CompanySearchResponse의 companyLogoUrl 방식 변경

--- src/main/java/com/nexerp/domain/company/model/response/CompanySearchResponse.java
+++ src/main/java/com/nexerp/domain/company/model/response/CompanySearchResponse.java
@@ -15,14 +15,14 @@ public class CompanySearchResponse {
   private final String imagePath;
 
   // 엔티티를 DTO로 변환하는 정적 팩토리 메서드
-  public static CompanySearchResponse from(Company company) {
+  public static CompanySearchResponse from(Company company, String companyLogoUrl) {
     return CompanySearchResponse.builder()
-        .id(company.getId())
-        .name(company.getName())
-        .industryType(company.getIndustryType())
-        .description(company.getDescription())
-        .imagePath(company.getImagePath())
-        .build();
+      .id(company.getId())
+      .name(company.getName())
+      .industryType(company.getIndustryType())
+      .description(company.getDescription())
+      .imagePath(companyLogoUrl)
+      .build();
   }
 
 }
