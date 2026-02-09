8633877c970b25a500a7a1a9e3aa68a031908832
zldzldzz <zldzldzz@gs.anyang.ac.kr>
2026-02-09 21:33:49 +0900

    refactor: openOutputStream 제거
    - S3Storage의 openOutputStream를 제거하기 위해 Port 제거

--- src/main/java/com/nexerp/domain/analytics/port/StoragePort.java
+++ src/main/java/com/nexerp/domain/analytics/port/StoragePort.java
@@ -1,7 +1,6 @@
 package com.nexerp.domain.analytics.port;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.List;
 
 // 파일이 저장될 공간(물리적 위치)을 관리하는 규칙
@@ -12,8 +11,6 @@ public interface StoragePort {
   String resolve(String fileName);
 
   String resolveTemp(String finalPath);
-
-  OutputStream openOutputStream(String tmpPath) throws IOException;
 
   void moveAtomic(String tmpPath, String finalPath) throws IOException;
 
